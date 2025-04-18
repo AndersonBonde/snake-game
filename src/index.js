4b22bc23570b9b9863424b353ddb5eb32d107f3e
Anderson Bonde <andersonbonde@gmail.com>
2025-04-18 12:43:43 -0300

    Move sketch to it's own module

--- src/index.js
+++ src/index.js
@@ -1,51 +1,5 @@
 import './styles.css';
 import p5 from 'p5';
-import createSnake from './scripts/createSnake';
-import createFood from './scripts/createFood';
+import createSketch from './scripts/sketch';
 
-const sketch = (p) => {
-  const size = 40;
-  const snake = createSnake(p, size);
-  let food;
-
-  p.setup = () => {
-    const canvas = p.createCanvas(400, 400);
-    canvas.parent(document.querySelector('main'));
-    p.frameRate(10);
-
-    food = createFood(p, size);
-  };
-
-  p.draw = () => {
-    p.background(60);
-    snake.checkDeath();
-    snake.update();
-    snake.show();
-    
-    if (snake.eat(food.getPos())) 
-      food.updatePos(snake.getBody());
-
-    food.show();
-  };
-
-  p.keyPressed = () => {
-    const { xSpeed, ySpeed } = snake;
-
-    switch (p.keyCode) {
-      case (p.UP_ARROW): 
-        if (ySpeed !== 1) snake.dir(0, -1); 
-        break;
-      case (p.DOWN_ARROW): 
-       if (ySpeed !== -1) snake.dir(0, 1); 
-       break;
-      case (p.RIGHT_ARROW): 
-        if (xSpeed !== -1) snake.dir(1, 0); 
-        break;
-      case (p.LEFT_ARROW): 
-        if (xSpeed !== 1) snake.dir(-1, 0); 
-        break;
-    }
-  }
-};
-
-new p5(sketch);
+new p5(createSketch);
---
+++ src/scripts/sketch.js
@@ -0,0 +1,49 @@
+const createSnake = require("./createSnake");
+const createFood = require("./createFood");
+
+function createSketch(p) {
+  const size = 40;
+  const snake = createSnake(p, size);
+  let food;
+
+  p.setup = () => {
+    const canvas = p.createCanvas(400, 400);
+    canvas.parent(document.querySelector('main'));
+    p.frameRate(10);
+
+    food = createFood(p, size);
+  };
+
+  p.draw = () => {
+    p.background(60);
+    snake.checkDeath();
+    snake.update();
+    snake.show();
+    
+    if (snake.eat(food.getPos())) 
+      food.updatePos(snake.getBody());
+
+    food.show();
+  };
+
+  p.keyPressed = () => {
+    const { xSpeed, ySpeed } = snake;
+
+    switch (p.keyCode) {
+      case (p.UP_ARROW): 
+        if (ySpeed !== 1) snake.dir(0, -1); 
+        break;
+      case (p.DOWN_ARROW): 
+       if (ySpeed !== -1) snake.dir(0, 1); 
+       break;
+      case (p.RIGHT_ARROW): 
+        if (xSpeed !== -1) snake.dir(1, 0); 
+        break;
+      case (p.LEFT_ARROW): 
+        if (xSpeed !== 1) snake.dir(-1, 0); 
+        break;
+    }
+  };
+}
+
+module.exports = createSketch;
